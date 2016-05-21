aba41eddf620cdbe373a2cfe31a4f5a59b9b948e
Vitor Mattos <vitor.mattos@phprio.org>
2016-05-20 21:55:31 -0300

    Organização de código

--- telegram.php
+++ telegram.php
@@ -27,118 +27,159 @@ $i = 0;
 $next = 'salvar';
 $command = null;
 $pause = false;
+$status = null;
+$config = [
+    '🚀Missões' => [
+        'text' => 'return @$history[$position]->text;',
+        'position' => [1],
+        'command' => function(&$command, $text, &$next) {
+            $command = $next == 'proteger'
+                ? '⭐️⭐️Proteger a caravana'
+                : '⭐️⭐️⭐️Salvar a vila';
+        }
+    ],
+    'start' => [
+        'text' => 'return @$history[$position]->text;',
+        'position' => [1],
+        'command' => function(&$command, $text, &$next) {
+            $command = in_array(strtolower($text), array('start'))
+                ? ($next == 'proteger'
+                    ? '⭐️⭐️Proteger a caravana'
+                    : '⭐️⭐️⭐️Salvar a vila'
+                  )
+                : $command;
+        }
+    ],
+    'stop' => [
+        'text' => 'return @$history[$position]->text;',
+        'position' => [0,1,2],
+        'command' => function(&$command, $text, &$next) {
+            $command = in_array(strtolower($text), array('stop', 'pause'))
+                ? 'stop'
+                : $command;
+        }
+    ],
+    'start_2' => [
+        'to_search' =>[
+            'Sua tropa foi destruída....',
+            'Sua tropa protegeu a caravana',
+            'Sua tropa veio para o resgate',
+            'Esses bandidos eram covardes',
+            'Excelente!',
+            'Você começou a trabalhar',
+            'Você pode lutar contra outros jogadores',
+            'Você pode escolher uma missão',
+            'Você já está trabalhando'
+        ],
+        'text' => 'return @$history[$position]->text;',
+        'position' => [2],
+        'command' => function(&$command, $text, &$next) {
+            $command = $next == 'proteger'
+                ? '⭐️⭐️Proteger a caravana'
+                : '⭐️⭐️⭐️Salvar a vila';
+        }
+    ],
+    'fazer missão' => [
+        'to_search' => ['Bandidos atacaram a vila.', 'Uma caravana passa perto de sua vila.'],
+        'text' => 'return @$history[$position]->media->caption;',
+        'position' => [2],
+        'command' => 'Fazer missão🗡'
+    ],
+    'mandar reforços' => [
+        'to_search' => [
+            'A caravana foi atacada e seus guardas mal conseguem',
+            'Durante a batalha, o inimigo levantou uma milícia',
+            'Seu time não foi suficiente',
+            'seus guardas mal conseguem',
+            'Os bandidos eram uns caras fortes'
+        ],
+        'text' => 'return @$history[$position]->text;',
+        'position' => [2],
+        'command' => function(&$command, $text, &$next) {
+            $next = $next == 'salvar' ? 'proteger' : 'salvar';
+            $command = 'Mandar reforços! 🗡';
+        }
+    ],
+    'atacar' => [
+        'to_search' => ['Seu inimigo é'],
+        'text' => 'return @$history[$position]->text;',
+        'position' => [2],
+        'command' => 'Atacar!⚔'
+    ],
+    [
+        'to_search' => ['Trabalho terminado, meu senhor!'],
+        'text' => 'return @$history[$position]->text;',
+        'position' => [0,1],
+        'command' => '/work'
+    ],
+    [
+        'to_search' => ['Trabalho terminado, meu senhor!', 'Você vendeu'],
+        'text' => 'return @$history[$position]->text;',
+        'position' => [2,2],
+        'command' => '/work'
+    ],
+    [
+        'to_search' => ['Seu campo está cheio'],
+        'text' => 'return @$history[$position]->text;',
+        'position' => [1,2],
+        'command' => '/harvest'
+    ],
+    [
+        'to_search' => ['Trabalhadores: '],
+        'text' => 'return @$history[$position]->text;',
+        'position' => [0,1,2],
+        'command' => function(&$command, $text, &$next) {
+            preg_match(
+                '/Nível (?<nivel>\d+)🏘, (?<percent>\d+(\.\d+)?%)[\s\S]*Ouro: (?<ouro>\d+)[\s\S]*Medalhas: (?<medalhas>\d+)/',
+                $text,
+                $matches
+            );
+            global $status;
+            $status = " Nível:{$matches['nivel']} {$matches['percent']} Ouro: {$matches['ouro']} Medalhas: {$matches['medalhas']}";
+        },
+        'break' => false
+    ]
+];
 while(true) {
     $history = $telegram->getHistory('$010000009348a00bc2714ae0add24a6c', 3);
-    if($history[1]->from->id == $user->id || $history[2]->from->id == $user->id) {
-        if($history[1]->text == "pause" || $history[1]->text == "stop") {
-            $command = null;
-            $pause = true;
-        }
-        if($history[1]->text == "start") {
-            $pause = false;
-        }
-        if(!$pause)
-        if(
-            strpos($history[1]->text, "🚀Missões") !== false ||
-            (
-                isset($history[2]->text) && 
-                strpos($history[2]->text, "Excelente!") !== false
-            )
-            ) {
-            if($next == 'proteger') {
-                $command = "⭐️⭐️Proteger a caravana";
-            } else {
-                $command = "⭐️⭐️⭐️Salvar a vila";
-            }
-        } else {
-            if(isset($history[2]->media, $history[2]->media->caption)) {
-                if(strpos($history[2]->media->caption, "Bandidos atacaram a vila.") !== false) {
-                    $command = "Fazer missão🗡";
-                } elseif(strpos($history[2]->media->caption, "Uma caravana passa perto de sua vila.") !== false) {
-                    $command = "Fazer missão🗡";
-                } else {
-                    $command = null;
-                }
-            } elseif(isset($history[2]->text)) {
-                if(
-                    strpos($history[2]->text, "seus guardas mal conseguem") !== false ||
-                    strpos($history[2]->text, "Os bandidos eram uns caras fortes") !== false
-                    ) {
-                    $next = $next == 'salvar' ? 'proteger' : 'salvar';
-                    $command = "Mandar reforços! 🗡";
-                } elseif(strpos($history[2]->text, "A caravana foi atacada e seus guardas mal conseguem") !== false) {
-                    $command = 'Mandar reforços! 🗡';
-                } elseif(strpos($history[2]->text, "Seu time não foi suficiente") !== false) {
-                    $command = 'Mandar reforços! 🗡';
-                } elseif(
-                    strpos($history[2]->text, "Sua tropa foi destruída....") !== false ||
-                    strpos($history[2]->text, "Sua tropa protegeu a caravana") !== false ||
-                    strpos($history[2]->text, "Sua tropa veio para o resgate") !== false ||
-                    strpos($history[2]->text, "Esses bandidos eram covardes") !== false
-                    ) {
-                    if($next == 'proteger') {
-                        $command = "⭐️⭐️Proteger a caravana";
-                    } else {
-                        $command = "⭐️⭐️⭐️Salvar a vila";
+    if($command == 'stop') {
+        $text = eval($config['start']['text']);
+        $command = $config['start']['command']($command, $text, $next);
+    }
+    if($command != 'stop') {
+        foreach($config as $to_search => $params) {
+            $to_search = isset($params['to_search'])
+                ? $params['to_search']
+                : [$to_search];
+            foreach($params['position'] as $position) {
+                $text = eval($params['text']);
+                foreach($to_search as $key) {
+                    if(strpos($text, $key) !== false) {
+                        if(is_callable($params['command'])) {
+                            $params['command']($command, $text, $next);
+                        } else {
+                            $command = $params['command'];
+                        }
+                        if(!isset($params['break']) || $params['break']) {
+                            break 3;
+                        }
                     }
-                } elseif(strpos($history[1]->text, "Seu campo está cheio") !== false) {
-                    $command = "/harvest";
-                } elseif(strpos($history[2]->text, "Seu campo está cheio") !== false) {
-                    $command = "/harvest";
-                } elseif(strpos($history[2]->text, "Você foi atacado por") !== false) {
-                    preg_match('/(?<command>\/revenge_\d+)/', $history[2]->text, $matches);
-                    $command = $matches['command'];
-                } elseif(strpos($history[2]->text, "Seu inimigo é") !== false) {
-                    $command = 'Atacar! ⚔';
-                } elseif(strpos($history[2]->text, 'Durante a batalha, o inimigo levantou uma milícia') !== false) {
-                    $command = 'Mandar reforços! 🗡';
-                } elseif(
-                    strpos($history[2]->text, 'Você pode lutar contra outros jogadores') !== false ||
-                    strpos($history[2]->text, 'Você pode escolher uma missão') !== false
-                    ) {
-                    if($next == 'proteger') {
-                        $command = "⭐️⭐️Proteger a caravana";
-                    } else {
-                        $command = "⭐️⭐️⭐️Salvar a vila";
-                    }
-                } elseif(strpos($history[2]->text, 'Você vendeu') !== false) {
-                    $command = "🍞Trabalhar!";
-                } elseif(strpos($history[2]->text, 'Trabalho terminado, meu senhor!') !== false) {
-                    $command = '/work';
-                } elseif(strpos($history[2]->text, 'Você começou a trabalhar') !== false) {
-                    if($next == 'proteger') {
-                        $command = "⭐️⭐️Proteger a caravana";
-                    } else {
-                        $command = "⭐️⭐️⭐️Salvar a vila";
-                    }
-                } elseif(strpos($history[2]->text, 'Trabalho terminado, meu senhor!') !== false) {
-                    $command = "🍞Trabalhar!";
-                } elseif(isset($history[1]->text) && strpos($history[1]->text, 'Trabalho terminado, meu senhor!') !== false) {
-                    $command = "🍞Trabalhar!";
-                } elseif(isset($history[0]->text) && strpos($history[0]->text, 'Trabalho terminado, meu senhor!') !== false) {
-                    $command = "🍞Trabalhar!";
-                } elseif($history[1]->text == "start") {
-                    if($next == 'proteger') {
-                        $command = "⭐️⭐️Proteger a caravana";
-                    } else {
-                        $command = "⭐️⭐️⭐️Salvar a vila";
-                    }
-                } else {
-                    $command = null;
                 }
             }
         }
     }
-    if($command) {
+    if($command && $command !== 'stop') {
         $telegram->msg('$010000009348a00bc2714ae0add24a6c', $command);
-        echo "$i -> $command -> $next\n";
+        echo "$i -> {$command}{$status} -> $next\n";
+        $command = null;
     } else {
-        if($pause) {
+        if($command == 'stop') {
             echo "$i -> pause -> $next\n";
         } else {
-            echo "$i -> no command -> $next\n";
+            echo "$i -> no command{$status} -> $next\n";
         }
     }
+    $status = null;
     $i++;
     sleep(rand(3, 5));
 }
